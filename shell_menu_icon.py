a6948958037a9ad3b75b225ca9be48cdb2e73cd5
Jose Luis Condori Jara <luiscondorijara@gmail.com>
2025-07-10 18:38:10 -0500

    Agregar instrucciones de uso para shell_menu_icon.py
    Se agregó una cadena de documentación en la parte superior del archivo con comandos para ejecutar el script en diferentes entornos de servidor, incluidos OdooSH, servidores configurados por Quanam y otros.

--- shell_menu_icon.py
+++ shell_menu_icon.py
@@ -1,3 +1,11 @@
+""" Comando para ejecutar desde la terminal del servidor:
+OdooSH:
+curl -s https://raw.githubusercontent.com/GrupoQuanam/utils/refs/heads/main/shell_menu_icon.py | odoo-bin shell
+Servidores configurados quanam:
+curl -s https://raw.githubusercontent.com/GrupoQuanam/utils/refs/heads/main/shell_menu_icon.py | odoo-shell
+Otros servidores:
+curl -s https://raw.githubusercontent.com/GrupoQuanam/utils/refs/heads/main/shell_menu_icon.py | python3 odoo-bin shell -c odoo.conf --no-http
+"""
 # Script para regenerar los iconos de los menus cuando restauras sin filestore
 menus = env['ir.ui.menu'].search([('web_icon', '!=', False)])
 for menu in menus:
