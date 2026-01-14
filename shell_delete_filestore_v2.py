6a1a05686f7730b5548de1613c61723c5cbdb12a
Jose Luis Condori Jara <luiscondorijara@gmail.com>
2026-01-14 16:45:38 -0500

    UPDATE shell_delete_filestore_v2.py

--- shell_delete_filestore_v2.py
+++ shell_delete_filestore_v2.py
@@ -1,20 +1,27 @@
 import os
 
+
+def dividir_en_bloques(array, tamano_bloque):
+    return [array[x:x + tamano_bloque] for x in range(0, len(array), tamano_bloque)]
+
+
 files = env['ir.attachment'].sudo().with_context(prefetch_fields=False).search(
     [('id', '>', 0), ('type', '=', 'binary')], order='id')
-total, i = len(files), 0
-files._read(['name'])
+to_delete = env['ir.attachment']
 for file in files.exists():
-    i += 1
-    position = f'{str(i).zfill(len(str(total)))} / {total}'
-    file_name = file.name
     full_path = file._full_path(file.store_fname)
-    if os.path.exists(full_path):
-        print(position, file.id, 'Existe', file_name)
-    else:
+    if not os.path.exists(full_path):
+        to_delete += file
+
+if to_delete:
+    lots = dividir_en_bloques(to_delete.ids, 100)
+    total, i = len(lots), 0
+    for lot in lots:
+        i += 1
+        position = f'{str(i).zfill(len(str(total)))} / {total} | {lot[0]}...{lot[-1]}'
         try:
-            file.unlink()
+            env['ir.attachment'].sudo().with_context(prefetch_fields=False).browse(lot).unlink()
             env.cr.commit()
         except:
-            pass
-        print(position, file.id, 'No existe', file_name)
+            env.cr.rollback()
+        print(position)
